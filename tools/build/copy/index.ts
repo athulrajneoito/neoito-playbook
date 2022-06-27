bc8e15d7bdd77350e420c70c4e238dc004eec000
NTPL-380 <athul.r@neoito.com>
2022-06-27 13:17:48 +0530

    home page design updated

--- tools/build/copy/index.ts
+++ tools/build/copy/index.ts
@@ -105,7 +105,7 @@ export function copyAll(
       mergeMap(file => copy({
         ...options,
         from: `${options.from}/${file}`,
-        to:   `${options.to}/${file.replace(/(\.{2}\/)+/, "")}`
+        to:   `${options.to}/${file}`
       }), 16)
     )
 }
